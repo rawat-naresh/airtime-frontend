5c6862d0af5a57cc9ce48143b3a3765d79921c76
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-25 18:11:48 +0530

    component file added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FollowerComponent } from './follower-helpers/follower/follower.component';
+import { FollowerListComponent } from './follower-helpers/follower-list/follower-list.component';
+import { FollowingComponent } from './following-helpers/following/following.component';
+import { FollowingListComponent } from './following-helpers/following-list/following-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { TweetComponent } from './tweet-helpers/tweet/tweet.component';
+import { TweetListComponent } from './tweet-helpers/tweet-list/tweet-list.component';
+import { TrendingComponent } from './trending-helpers/trending/trending.component';
+import { TrendingListComponent } from './trending-helpers/trending-list/trending-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FollowerComponent, FollowerListComponent, FollowingComponent, FollowingListComponent, FooterComponent, HeaderComponent, TweetComponent, TweetListComponent, TrendingComponent, TrendingListComponent]
 })
 export class SharedModule { }
---
+++ src/app/shared/trending-helpers/trending-list/trending-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-list',
+  templateUrl: './trending-list.component.html',
+  styleUrls: ['./trending-list.component.css']
+})
+export class TrendingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

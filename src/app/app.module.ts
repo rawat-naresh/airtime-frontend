3f187820cf0dfbe112048b728b840bca21f6dc60
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-26 18:38:54 +0530

    shared module and few other modules added to the imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+/*  */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* component imports */
 import { AppComponent } from './app.component';
+
+/* Feature module imports */
+import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from 'src/app/app.routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    HomeModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

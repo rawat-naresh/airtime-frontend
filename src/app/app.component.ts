cce4442fbcb9a4aac6f9eec0a6a0470106d89d2a
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-27 12:52:15 +0530

    call userService.populate() method on app startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './core/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'airtime-frontend';
+  constructor(
+    private userService:UserService
+  ){}
+  ngOnInit() {
+    /**
+     * When the user first launches the app
+     * populate method will be executed
+     * based on the token stored in local storage, it willl fetch data
+     * and set the authenticatedSubject
+     */
+    this.userService.populate();
+  }
 }

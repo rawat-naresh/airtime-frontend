1b8f1e14bb5203bd6b0af9a3a454f925fa7e97d4
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-27 12:54:07 +0530

    API service created

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+/* Developnment environment */
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ApiService{
+
+    constructor(private http:HttpClient) {}
+
+    private formatErrors(error:any) {
+        return throwError(error.error);
+    }
+
+    get(path:string, params:HttpParams = new HttpParams()):Observable<any>{
+        return this.http.get(`${environment.api_url}${path}`,{params})
+            .pipe(catchError(this.formatErrors));
+    }
+
+    put(path:string, body:Object = {}):Observable<any> {
+        return this.http.put(`${environment.api_url}${path}`,JSON.stringify(body))
+            .pipe(catchError(this.formatErrors));
+    }
+
+    post(path:string, body:Object = {}):Observable<any> {
+        return this.http.post(`${environment.api_url}${path}`,JSON.stringify(body))
+            .pipe(catchError(this.formatErrors));
+    }
+
+    delete(path:string):Observable<any> {
+        return this.http.delete(`${environment.api_url}${path}`)
+            .pipe(catchError(this.formatErrors));
+    }
+}

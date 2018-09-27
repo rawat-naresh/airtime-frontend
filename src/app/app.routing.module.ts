6c2b7e3fd83dd61c9407fa696ee5e5893f2242f2
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-27 18:30:40 +0530

    signup route is added

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes:Routes = [
+    {path:'', pathMatch:'full',redirectTo:'signup'},
+    {path:'signup', loadChildren:"./signup/signup.module#SignupModule"}
+    
 ];
 @NgModule({
     imports:[RouterModule.forRoot(appRoutes)],
     exports:[RouterModule]
 })
 export class AppRoutingModule {
-
+    
 }

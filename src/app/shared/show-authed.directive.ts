0e99c4d913aae8917ae1fa4d4d082dc69f4f0616
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-27 12:55:16 +0530

    modified to check whether the user is authenticated or not

--- src/app/shared/show-authed.directive.ts
+++ src/app/shared/show-authed.directive.ts
@@ -1,19 +1,35 @@
 import { Directive, ViewContainerRef, TemplateRef, OnInit, Input } from "@angular/core/";
+import { UserService } from '../core/services/user.service';
 
 @Directive({
     selector:'[appShowAuthed]'
 })
 export class ShowAuthedDirective implements OnInit{
     condition:boolean;
-    constructor(private templateRef:TemplateRef<any>,private viewContainer:ViewContainerRef){}
+    constructor(
+        private templateRef:TemplateRef<any>,
+        private viewContainer:ViewContainerRef,
+        private userService:UserService
+    ){}
 
     ngOnInit(){
-        if(this.condition) {
+        this.userService.isAuthenticated.subscribe(
+            (isAuthenticated) => {
+                if(isAuthenticated && this.condition || !isAuthenticated && !this.condition) {
+                    this.viewContainer.createEmbeddedView(this.templateRef);
+            
+                } else {
+                    this.viewContainer.clear();
+            
+                }
+            }
+        );
+        /* if(!this.condition && this.userService.) {
             this.viewContainer.createEmbeddedView(this.templateRef);
         }
         else {
             this.viewContainer.clear();
-        }
+        } */
     }
 
     @Input() set appShowAuthed(condition:boolean) {

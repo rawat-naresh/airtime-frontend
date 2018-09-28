55cf7f55251285122b503cf4164e90f9689555a8
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-28 18:33:37 +0530

    new modules imported

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { SignupComponent } from './signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignupRoutingModule } from './signup.routing.module'
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   imports: [
+    CommonModule,
     ReactiveFormsModule,
-    SignupRoutingModule
+    SignupRoutingModule,
+    SharedModule
   ],
   declarations: [
       SignupComponent

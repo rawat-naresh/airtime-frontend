71278e9f1ca00c512ee3f14dc91248f51cb38995
Naresh Rawat <nareshrawat33@gmail.com>
2018-09-28 18:29:29 +0530

    root path  removed form appRouting

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes:Routes = [
-    {path:'', pathMatch:'full',redirectTo:'signup'},
     {path:'signup', loadChildren:"./signup/signup.module#SignupModule"}
     
 ];

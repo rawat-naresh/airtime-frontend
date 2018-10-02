1d1fec769153e7fc37a6e867e62478e2b01534ac
unknown <nareshrawat33@gmail.com>
2018-10-02 23:46:44 +0530

    profile module created

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileRoutingModule } from './profile.routing.module';
+import { ProfileComponent } from './profile.component';
+import { SharedModule } from '../shared/shared.module';
+import { ProfileResolver } from './profile-resolver.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule,
+    ProfileRoutingModule,
   ],
-  declarations: []
+  declarations: [
+    ProfileComponent
+  ],
+  providers:[ProfileResolver]
 })
 export class ProfileModule { }

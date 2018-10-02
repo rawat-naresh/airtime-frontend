0ce30af081f527c0b60c1b30e43a5ce10fca76c5
unknown <nareshrawat33@gmail.com>
2018-10-02 23:43:21 +0530

    profile service created

---
+++ src/app/core/services/profiles.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ApiService } from './api.service';
+import { Profile } from '../model/profile.model';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProfilesService {
+  constructor (
+    private apiService: ApiService
+  ) {}
+
+  get(username: string): Observable<Profile> {
+    return this.apiService.get('/users/' + username)
+      .pipe(map((data/*: {profile: Profile}*/) => data.profile));
+  }
+
+  follow(username: string): Observable<Profile> {
+    return this.apiService.post('/users/' + username + '/follow');
+  }
+
+  unfollow(username: string): Observable<Profile> {
+    return this.apiService.delete('/users/' + username + '/follow');
+  }
+
+}

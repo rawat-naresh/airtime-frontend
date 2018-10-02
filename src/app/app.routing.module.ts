60dfc2f0ead5873ad94a42c157074ff092ca3801
unknown <nareshrawat33@gmail.com>
2018-10-02 23:41:53 +0530

    added profile route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes:Routes = [
-    {path:'signup', loadChildren:"./signup/signup.module#SignupModule"}
+    {path:'signup', loadChildren:"./signup/signup.module#SignupModule"},
+    {path:'profile', loadChildren:"./profile/profile.module#ProfileModule"},
+
     
 ];
 @NgModule({

a8a21dbbe1ffe395a7c3673f1180c42b0b567646
unknown <nareshrawat33@gmail.com>
2018-10-02 23:50:58 +0530

    sugnup module created

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { SignupComponent } from './signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignupRoutingModule } from './signup.routing.module'
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   imports: [
-    CommonModule,
+    //CommonModule,
     ReactiveFormsModule,
     SignupRoutingModule,
     SharedModule

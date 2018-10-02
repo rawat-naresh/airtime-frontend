df9bfd75f5f44caf58e0402c41fa117bb4035a65
unknown <nareshrawat33@gmail.com>
2018-10-02 23:42:16 +0530

    added profile service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http'; 
 import { HttpTokenInterceptor } from './interceptors/http.token.interceptor';
 import { ApiService } from './services/api.service';
@@ -7,12 +7,13 @@ import { JwtService } from './services/jwt.service';
 import { UserService } from './services/user.service';
 import { TweetService } from './services/tweet.service';
 import {MessageService} from 'primeng/api';
+import { ProfilesService } from './services/profiles.service';
 
 
 
 @NgModule({
   imports: [
-    CommonModule
+    // CommonModule
   ],
   declarations: [],
   providers:[
@@ -21,6 +22,7 @@ import {MessageService} from 'primeng/api';
     JwtService,
     UserService,
     TweetService,
+    ProfilesService,
     MessageService,
   ]
 })

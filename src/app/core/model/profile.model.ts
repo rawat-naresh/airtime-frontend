f2df8bd6f1e384d10d429e124e18198e6b547e8c
unknown <nareshrawat33@gmail.com>
2018-10-02 23:42:46 +0530

    profile model created

---
+++ src/app/core/model/profile.model.ts
@@ -0,0 +1,17 @@
+import { Tweet } from "./tweet.model";
+
+export interface Profile {
+    firstname: string,
+    lastname: string,
+    username: string,
+    bio: string,
+    birthday: Date,
+    contact: string,
+    relationship: string,
+    profile: string,
+    wall: string,
+    followersCount: number,
+    followingCount: number,
+    tweets: Tweet,
+    createdAt: Date,
+}

f0abd1920f7d8c9e48e08a1fc430f2f514ee5260
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-03 18:32:58 +0530

    files moved

--- src/app/shared/tweet-helpers/tweet/tweet-splitter.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'tweetSplitter' })
-export class TweetSplitterPipe implements PipeTransform {
-  transform(tweet) {
-    return tweet.split(" ");
-  }
-}

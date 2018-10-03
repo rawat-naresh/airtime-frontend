171b7ed2b7fb6b7b003e4853d38ac357389f3e74
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-03 18:21:37 +0530

    tweet model updated

--- src/app/core/model/tweet.model.ts
+++ src/app/core/model/tweet.model.ts
@@ -1,12 +1,17 @@
 export interface Tweet {
-    
-        _id:string,
-        body:string,
-        likesCount:number,
-        reTweetsCount:number,
-        commentsCount:number,
-        attachments:string[],
-        isLiked: boolean,
-        createdAt:Date
+        firstname:string,
+        lastname:string,
+        username;string,
+        tweet:{
+                _id:string,
+                body:string,
+                likesCount:number,
+                reTweetsCount:number,
+                commentsCount:number,
+                attachments:string[],
+                isLiked: boolean,
+                isRetweeted:boolean,
+                createdAt:Date
+        }
     
 }

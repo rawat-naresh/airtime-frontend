a8275619a06cb8ea31d4c5a55a6d9cc9786c044d
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-04 14:43:55 +0530

    user intro component created

---
+++ src/app/shared/user-intro/user-intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-intro',
+  templateUrl: './user-intro.component.html',
+  styleUrls: ['./user-intro.component.css']
+})
+export class UserIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

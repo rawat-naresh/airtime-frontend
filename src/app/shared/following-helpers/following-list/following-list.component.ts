86a1debf77e7015832b0786b5414929640cfbed0
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-05 17:23:37 +0530

    following and following list component created

--- src/app/shared/following-helpers/following-list/following-list.component.ts
+++ src/app/shared/following-helpers/following-list/following-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-following-list',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./following-list.component.css']
 })
 export class FollowingListComponent implements OnInit {
-
-  constructor() { }
+  followingList:Object[];
+  username:string;
+  constructor(
+    private userService:UserService,
+    private route:ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.route.parent.params.subscribe((params)=>{
+      this.username = params['username'];
+    });
+    this.userService.getFollowings(this.username).subscribe(
+      (data) => {
+        this.followingList = data;
+      }
+    );
+  }
+
+  removeFollowing(user) {
+    let index = this.followingList.indexOf(user);
+    this.followingList.splice(index,1);
+  }
+
+  addFollowing(user) {
+    //let index = this.followingList.indexOf(user);
+    this.followingList.push(user);
   }
 
 }
--- src/app/shared/following-helpers/following/following.component.ts
+++ src/app/shared/following-helpers/following/following.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, Output ,EventEmitter} from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
+import { ProfilesService } from 'src/app/core/services/profiles.service';
 
 @Component({
   selector: 'app-following',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./following.component.css']
 })
 export class FollowingComponent implements OnInit {
-
-  constructor() { }
+  @Input() user:Object;
+  @Output() userFollowEvent = new EventEmitter<Object>();
+  @Output() userUnfollowEvent = new EventEmitter<Object>();
+  
+  constructor(
+    private userService:UserService,
+    private profileService:ProfilesService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  unfollowUser(username){
+    this.userService.unfollowUser(username).subscribe(
+      data => {
+        this.user.isFollowing = false;
+        
+        this.userUnfollowEvent.emit(this.user);
+        this.profileService.followingReplaySubject.next(data);
+      }
+    );
+  }
+
+  followUser(username) {
+    // console.log("dfd");
+    this.userService.followUser(username).subscribe(
+      (data) => {
+        this.user.isFollowing = true;
+        //get following count in data.
+        this.userFollowEvent.emit(this.user);
+        this.profileService.followingReplaySubject.next(data);
+      }
+    );
   }
 
 }

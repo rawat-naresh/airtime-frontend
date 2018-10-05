a97d65366ff587409924c94bfdeb1d0800abe7bb
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-05 17:27:18 +0530

    user intro component created

--- src/app/shared/user-intro/user-intro.component.ts
+++ src/app/shared/user-intro/user-intro.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Profile } from 'src/app/core/model/profile.model';
+import { UserService } from 'src/app/core/services/user.service';
+import { User } from 'src/app/core/model/user.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-intro',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-intro.component.css']
 })
 export class UserIntroComponent implements OnInit {
-
-  constructor() { }
+  @Input() profile:Profile;
+  currentUser:string
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
+    this.userService.currentUser.subscribe(
+      data => {
+        this.currentUser = data.username;
+      }
+    )
   }
 
 }

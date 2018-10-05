d845a99467e959c4fa6af5513560b218230ec0e6
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-05 17:26:08 +0530

    updated to use followingReplaySubject

--- src/app/shared/suggestion-helpers/suggestion/suggestion.component.ts
+++ src/app/shared/suggestion-helpers/suggestion/suggestion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
 import { UserService } from '../../../core/services/user.service';
+import { ProfilesService } from 'src/app/core/services/profiles.service';
 
 @Component({
   selector: 'app-suggestion',
@@ -7,11 +8,11 @@ import { UserService } from '../../../core/services/user.service';
 //   styleUrls: ['./suggestion-list.component.css']
 })
 export class SuggestionComponent implements OnInit {
-  isFollowed:boolean = false;
   @Input() suggestion:Object;
   @Output() followed = new EventEmitter<Object>();
   constructor(
-    private userService:UserService
+    private userService:UserService,
+    private profileService:ProfilesService
   ) { }
 
   ngOnInit() {
@@ -22,8 +23,8 @@ export class SuggestionComponent implements OnInit {
     this.userService.followUser(username).subscribe(
       (data) => {
         //get following count in data.
-        this.isFollowed = true;
         this.followed.emit(this.suggestion);
+        this.profileService.followingReplaySubject.next(data);
       }
     );
   }
